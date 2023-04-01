8ae89cfe4af9a9b20aa0b3ecbc1d42be26716f96
Lilis Lisnawati <125554211+htomom@users.noreply.github.com>
2023-04-01 15:06:56 +0700

    miniapp trouble

--- miniapp.py
+++ miniapp.py
@@ -1,11 +1,62 @@
+# import module
 import pandas as pd
 import re
-from flask_cors import request
+from flask import Flask, jsonify
+from flask import request
 from flasgger import Swagger, LazyString, LazyJSONEncoder
 from flasgger import swag_from
-from flask import Flask, request, jsonify
-import fungsi
 
+app = Flask(__name__)
+
+# connect data abusive dan kamus alay
+df_abusive = pd.read_csv("D:/Binar/Challenge/BinarChallenge1/AssetChallenge/abusive.csv")
+df_abusive
+
+df_kbbi = pd.read_csv("D:/Binar/Challenge/BinarChallenge1/AssetChallenge/new_kamusalay.csv", encoding='latin-1', names=['TIDAKBAKU', 'BAKU'])
+df_kbbi
+
+# fungsi cleaning
+def lowercase(text):
+    return text.lower()
+
+def removechars(text):
+    text = re.sub('\n',' ',text) 
+    text = re.sub('rt',' ',text) 
+    text = re.sub('user',' ',text) 
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ',text) 
+    text = re.sub('  +', ' ', text) 
+    return text
+    
+def removenothers(text):
+    text = re.sub('[^0-9a-zA-Z~*]+', ' ', text) 
+    return text
+
+def cleaning(text):
+    text = lowercase(text)
+    text = removechars(text)
+    text = removenothers(text)
+    return text
+
+
+# fungsi censor kata kasar dan ubah kalimat alay
+def censor(text):
+    abusiveword = connectcsv.df_abusive['ABUSIVE'].tolist()
+    for word in abusiveword:
+        pattern = re.compile(r'\b{}\b'.format(word))
+        length = len(word)
+        replacement = '*' * length
+        text = pattern.sub(replacement, text.lower())
+    return text
+
+def changealay(txt):
+    alay = dict(zip(connectcsv.df_kbbi['TIDAKBAKU'], connectcsv.df_kbbi['BAKU']))
+    txt = ' '.join([alay[word] if word in alay else word for word in txt.split(' ')])
+    return txt
+
+
+# --- start swagger---
+
+# aplikasi swagger 
 app.json_encoder = LazyJSONEncoder
 swagger_template = dict(
 info = {
@@ -31,27 +82,48 @@ swagger = Swagger(app, template=swagger_template,
                   config=swagger_config)
 
 
+# endpoint 1
+@swag_from("docs/cleaningabusive.yml", methods=['GET'])
+@app.route('/cleaningabusive', methods=['GET'])
+def text_clean():
+    json_response = {
+        'status_code': 200,
+        'description': "Hasil teks yang sudah dibersihkan",
+        'data': print(cleaning(censor(text)))
+    }
 
+    response_data = jsonify(json_response)
+    return response_data
 
+# endpoint 2
+@swag_from("docs/cleaningkbbi.yml", methods=['POST'])
+@app.route('/cleaningkbbi', methods=['POST'])
+def processing_file():
 
+    # Uploaded file
+    file = request.files.getlist('file')[0]
 
-app = Flask(__name__)
+    # Import file csv ke Pandas
+    df = pd.read_csv(file)
 
+    # Ambil teks yang akan diproses dalam format list
+    texts = df.text.to_list()
 
-# @app.route("/")
-# def getData():
-#     objgetData = getdata.getDatas()
-#     data = objgetData.getAllData()
-#     if len(data):
-#         response = jsonify({
-#             "result" : data,
-#             "status" : 200,
-#         })
-#     elif:
-#         response = jsonify({
-#             "result" : [],
-#             "status" : 400,
-#         })
-#     return response
+    # Lakukan cleansing pada teks
+    cleaned_text = []
+    for text in texts:
+        cleaned_text.append(changealay(txt))
 
-# app.run("0.0.0.0", debug=True)
+    json_response = {
+        'status_code': 200,
+        'description': "Teks yang sudah diproses",
+        'data': print(cleaning(changealay(txt)))
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
+if __name__ == '__main__':
+   app.run()
+
+# --- end swagger ---
