457e2d73451b49e1d40c9cf00f4b2fd8237ea0d6
Lilis Lisnawati <125554211+htomom@users.noreply.github.com>
2023-04-02 04:43:27 +0700

    final code

--- miniapp.py
+++ miniapp.py
@@ -15,32 +15,31 @@ df_abusive
 df_kbbi = pd.read_csv("D:/Binar/Challenge/BinarChallenge1/AssetChallenge/new_kamusalay.csv", encoding='latin-1', names=['TIDAKBAKU', 'BAKU'])
 df_kbbi
 
+
 # fungsi cleaning
 def lowercase(text):
     return text.lower()
 
 def removechars(text):
     text = re.sub('\n',' ',text) 
-    text = re.sub('rt',' ',text) 
+    text = re.sub('RT',' ',text) 
     text = re.sub('user',' ',text) 
-    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ',text) 
-    text = re.sub('  +', ' ', text) 
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ',text)
+    text = re.sub('  +', ' ', text)  
     return text
-    
+
 def removenothers(text):
-    text = re.sub('[^0-9a-zA-Z~*]+', ' ', text) 
+    text = re.sub('[^0-9a-zA-Z~*#]+', ' ', text) 
     return text
 
-def cleaning(text):
-    text = lowercase(text)
-    text = removechars(text)
-    text = removenothers(text)
+# fungsi cleaning censor kata kasar dan ubah kalimat alay
+def changealay(text):
+    alay = dict(zip(df_kbbi['TIDAKBAKU'], df_kbbi['BAKU']))
+    text = ' '.join([alay[word] if word in alay else word for word in text.split(' ')])
     return text
 
-
-# fungsi censor kata kasar dan ubah kalimat alay
 def censor(text):
-    abusiveword = connectcsv.df_abusive['ABUSIVE'].tolist()
+    abusiveword = df_abusive['ABUSIVE'].tolist()
     for word in abusiveword:
         pattern = re.compile(r'\b{}\b'.format(word))
         length = len(word)
@@ -48,11 +47,14 @@ def censor(text):
         text = pattern.sub(replacement, text.lower())
     return text
 
-def changealay(txt):
-    alay = dict(zip(connectcsv.df_kbbi['TIDAKBAKU'], connectcsv.df_kbbi['BAKU']))
-    txt = ' '.join([alay[word] if word in alay else word for word in txt.split(' ')])
-    return txt
 
+def cleaning(text):
+    text = removechars(text)
+    text = lowercase(text)
+    text = removenothers(text)
+    text = changealay(text)
+    text = censor(text)
+    return text
 
 # --- start swagger---
 
@@ -83,41 +85,42 @@ swagger = Swagger(app, template=swagger_template,
 
 
 # endpoint 1
-@swag_from("docs/cleaningabusive.yml", methods=['GET'])
-@app.route('/cleaningabusive', methods=['GET'])
+@swag_from("text_processing.yml", methods=['POST'])
+@app.route('/text_processing', methods=['POST'])
 def text_clean():
+
+    text = request.form.get('text')
+
     json_response = {
         'status_code': 200,
         'description': "Hasil teks yang sudah dibersihkan",
-        'data': print(cleaning(censor(text)))
+        'data': cleaning(text)
     }
 
     response_data = jsonify(json_response)
     return response_data
 
 # endpoint 2
-@swag_from("docs/cleaningkbbi.yml", methods=['POST'])
-@app.route('/cleaningkbbi', methods=['POST'])
+@swag_from("text_processing_file.yml", methods=['POST'])
+@app.route('/text_processing_file', methods=['POST'])
 def processing_file():
 
     # Uploaded file
     file = request.files.getlist('file')[0]
 
     # Import file csv ke Pandas
-    df = pd.read_csv(file)
+    df = pd.read_csv(file, encoding='latin-1')
 
-    # Ambil teks yang akan diproses dalam format list
-    texts = df.text.to_list()
 
     # Lakukan cleansing pada teks
     cleaned_text = []
-    for text in texts:
-        cleaned_text.append(changealay(txt))
+    for text in df['Tweet']:
+        cleaned_text.append(cleaning(text))
 
     json_response = {
         'status_code': 200,
         'description': "Teks yang sudah diproses",
-        'data': print(cleaning(changealay(txt)))
+        'data': cleaned_text
     }
 
     response_data = jsonify(json_response)
